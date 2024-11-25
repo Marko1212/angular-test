9b52a62462881572b0aca3b4af78fdf700cc6805
Marko Askovic <marko.askovic@astree-software.fr>
2024-11-25 23:41:01 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-area',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ReactiveFormsModule, CommonModule],
+  template:
+    '' +
+    '<div>' +
+    '<form [formGroup]="usernameForm" (ngSubmit)="generateUsername()">' +
+    '<input type="text" formControlName="firstname" id="firstname">' +
+    '<input type="text" formControlName="lastname" id="lastname">' +
+    '<button type="submit">Generate</button>' +
+    '</form>' +
+    '<span id="output">generated username...</span>' +
+    '<p *ngIf="username">{{username}}</p>' +
+    '</div>',
+  styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-test';
+
+  usernameForm!: FormGroup;
+  username: string | undefined;
+
+  ngOnInit(): void {
+    this.usernameForm = new FormGroup({
+      firstname: new FormControl('Coder', Validators.required),
+      lastname: new FormControl('Byte', Validators.required),
+    });
+  }
+
+  generateUsername(): void {
+    const firstname = this.usernameForm?.get('firstname')?.value;
+    const lastname = this.usernameForm?.get('lastname')?.value;
+    const randomInit = Math.floor(Math.random() * 9) + 1;
+    this.username = `${firstname.toLowerCase()}_${lastname.toLowerCase()}_${randomInit}`;
+  }
 }
